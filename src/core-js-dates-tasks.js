aed28e407216aa77b2910ec015fb7403e54a9e1d
NatashaKle <Ataska91@gmail.com>
2024-03-25 17:20:44 +0300

    feat: finish 4 tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -61,8 +61,11 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const currentDay = date.getDay();
+  const daysUntilNextFriday = currentDay === 5 ? 7 : (5 - currentDay + 7) % 7;
+  date.setDate(date.getDate() + daysUntilNextFriday);
+  return date;
 }
 
 /**

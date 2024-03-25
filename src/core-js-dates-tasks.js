33ea6d7f70e41dd25e0960588cad02271ba52e6c
Natashakle <ataska91@gmail.com>
2024-03-25 20:36:20 +0300

    fix: fix 2 task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -32,7 +32,10 @@ function dateToTimestamp(date) {
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
 function getTime(date) {
-  return date.toLocaleTimeString('ru-RU', { hour12: false });
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
 }
 
 /**
